6669f6348426c05a317a238c6c7a2a936791360a
pppp <ppp@users.noreply.github.com>
2021-12-30 15:15:03 +0700

    Name should be in format

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -1,5 +1,6 @@
 class BabyName < ApplicationRecord
   belongs_to :name_list
-  validates :name, presence: true
+  validates :name, presence: true,
+                   format: { with: /\A[a-zA-Z]*\s{0,1}[a-zA-Z]*\Z/i, message: I18n.t('baby_name.format') }
   validates :name, uniqueness: { scope: :name_list_id, message: I18n.t('baby_name.uniq_name') }
 end
--- spec/requests/api/baby_names_spec.rb
+++ spec/requests/api/baby_names_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'Api::BabyNames', type: :request do
     let(:name_list) { create(:name_list) }
     context 'with valid params' do
       it 'adds a baby names to a name list' do
-        post '/api/baby_names', params: { baby_name: {name: " trim both ends ", name_list_id: name_list.id } }
+        post '/api/baby_names', params: { baby_name: {name: " trim both ", name_list_id: name_list.id } }
 
         expect(response).to have_http_status(201)
         body = JSON.parse(response.body).with_indifferent_access
         expect(body[:name_list_id]).to eq name_list.id
-        expect(body[:name]).to eq "trim both ends"
+        expect(body[:name]).to eq "trim both"
       end
     end
 
@@ -25,6 +25,14 @@ RSpec.describe 'Api::BabyNames', type: :request do
         expect(response).to have_http_status(422)
         body = JSON.parse(response.body).with_indifferent_access
         expect(body[:error]).to eq "Name is already existed in this list."
+      end
+
+      it 'returns errors when name is not in format' do
+        post '/api/baby_names', params: { baby_name: { name: "H3ello", name_list_id: name_list.id } }
+
+        expect(response).to have_http_status(422)
+        body = JSON.parse(response.body).with_indifferent_access
+        expect(body[:error]).to eq "Name should be formated with letters and (at most) one space."
       end
     end
   end
